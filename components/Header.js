8be9bfa80d63faf7f8942546a9b194015e809c00
Josh Head <jhead12@users.noreply.github.com>
2025-01-05 16:19:10 -0800

    blog 2

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
+import Image from 'next/image'; // Import the Next.js Image component
+import logo from 'components/numberlessagaing_logo.png'; // Import the image file
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="block w-20 h-12 mx-auto mb-5">
+        <Image src={logo} alt="Numberless Aging Logo" width={100} height={100} />
+      </div>
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
           {name}
