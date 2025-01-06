dd8975a2b66871ba1ac3c8bb0ff1257505ef7588
Josh Head <jhead12@users.noreply.github.com>
2025-01-05 20:32:33 -0800

    update

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
       <div className="block w-20 h-12 mx-auto mb-5">
-        <Image src={logo} alt="Numberless Aging Logo" width={100} height={100} />
+      <Link href="/"><Image src={logo} alt="Numberless Aging Logo" width={100} height={100} /></Link> 
       </div>
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
