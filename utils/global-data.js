fe6495bf037d655f59b0e459d1e28799a43305b5
Josh Head <jhead12@users.noreply.github.com>
2025-01-05 16:30:18 -0800

    quoutes

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Numberless Aging';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Age Aint Nothing But A Number';
+    : '"Age Aint Nothing But A Number"';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
